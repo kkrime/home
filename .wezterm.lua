54700c57ec8a2580fd4ca61d47fde189ba0e68f6
Iraq Jaber <IraqJaber@gmail.com>
2024-02-06 09:11:38 +0000

    added wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,28 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+config.font_size = 8
+
+
+config.keys = {
+  {
+    key = 'n',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
+config.color_scheme = 'Kanagawa (Gogh)'
+
+-- config.font = wezterm.font 'Fira Code'
+
+-- and finally, return the configuration to wezterm
+return config
