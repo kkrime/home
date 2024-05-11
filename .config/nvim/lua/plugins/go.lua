4625de697483f7b4e8547c389a39b5a17c29fa45
Iraq Jaber <IraqJaber@gmail.com>
2024-05-11 13:40:39 +0100

    added code to save all go files before running :GoBuild in vim

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -11,9 +11,18 @@ return {
       require("go").setup()
 
       vim.keymap.set("n", "<C-b>", function()
+        -- loop though and save .go files only
+        for i, buf_hndl in ipairs(vim.api.nvim_list_bufs()) do
+          -- file = vim.api.nvim_buf_get_name(buf_hndl)
+          fileType = vim.fn.getbufvar(buf_hndl, "&filetype")
+          if fileType == "go" then
+            vim.api.nvim_buf_call(buf_hndl, function()
+                vim.cmd('w')
+            end)
+          end
+        end
         vim.api.nvim_command([[:GoBuild]])
-      end
-      , { silent = true, noremap = true })
+      end, { silent = true, noremap = true })
 
       vim.keymap.set("n", "<C-y>", function()
         vim.api.nvim_command([[:GoTest -F]])
