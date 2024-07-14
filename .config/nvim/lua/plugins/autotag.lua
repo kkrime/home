8bb9576f8001855a743bd762c78869922281f576
Iraq Jaber <IraqJaber@gmail.com>
2024-07-14 11:02:30 +0100

    misc

--- .config/nvim/lua/plugins/autotag.lua
+++ .config/nvim/lua/plugins/autotag.lua
@@ -1,10 +1,11 @@
 return {
   {
     "windwp/nvim-ts-autotag",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
+    -- version = "*", -- Use for stability; omit to use `main` branch for the latest features
     config = function()
-      require('nvim-ts-autotag').setup()
+      require('nvim-ts-autotag').setup({
+        -- filetypes = { "html", "xml" },
+      })
     end,
   },
 }
