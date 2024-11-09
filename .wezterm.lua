7d48b5cc360501c5c7faf43f191e924814837c5a
Iraq Jaber <IraqJaber@gmail.com>
2024-11-09 12:23:20 +0800

    confiured wezterm to not prompt when closing tabs

--- .wezterm.lua
+++ .wezterm.lua
@@ -20,9 +20,24 @@ config.keys = {
   },
 }
 
+config.skip_close_confirmation_for_processes_named = {
+  'bash',
+  'sh',
+  'zsh',
+  'fish',
+  'tmux',
+  'nu',
+  'cmd.exe',
+  'pwsh.exe',
+  'powershell.exe',
+  'zellij',
+}
+
 config.color_scheme = 'Kanagawa (Gogh)'
 
 -- config.font = wezterm.font 'Fira Code'
 
 -- and finally, return the configuration to wezterm
+
+-- config.window_close_confirmation = 'NeverPrompt'
 return config
