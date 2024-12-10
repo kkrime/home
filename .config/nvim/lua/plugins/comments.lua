a2f0d5e44116c286dc3e1575e5c7805e6f1714ab
Iraq Jaber <IraqJaber@gmail.com>
2024-12-10 08:22:17 +0800

    added 'gcn' to copy current line to line below and comment out current line

--- .config/nvim/lua/plugins/comments.lua
+++ .config/nvim/lua/plugins/comments.lua
@@ -8,9 +8,24 @@ return {
     require('Comment').setup {
       ignore = '^$',
     }
+
     vim.keymap.set("n", "gcl", function()
       vim.api.nvim_feedkeys("gcc", 'm', false)
-    end
-    , { silent = true, noremap = true })
+    end, { silent = true, noremap = true })
+
+    vim.keymap.set("n", "gcn", function()
+      if vim.api.nvim_get_current_line() == "" then
+        return
+      end
+      -- get curosr location
+      local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+      -- get line
+      vim.api.nvim_feedkeys("\"ayy", 'x', true)
+      vim.api.nvim_feedkeys("gcl", 'x', true)
+      -- paste line
+      vim.api.nvim_feedkeys("\"ap", 'x', true)
+      -- set curosr location
+      vim.api.nvim_win_set_cursor(0, { row + 1, col })
+    end, { silent = true, noremap = true })
   end
 }
