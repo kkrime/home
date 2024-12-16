8c8cda76de2a32898b1af0c5979d4cd7880f922a
Iraq Jaber <IraqJaber@gmail.com>
2024-12-16 10:39:40 +0800

    added tree sitter playground

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -14,7 +14,28 @@ return {
           enable = true,
         },
         indent = { enable = true },
+
+        playground = {
+          enable = true,
+          updatetime = 25,         -- Debounced time for highlighting nodes in the playground
+          persist_queries = false, -- Persist queries across sessions
+          keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+          },
+        }
       }
     end
+  },
+  {
+    'nvim-treesitter/playground',
   },
 }
