58a19dedfad3e088adca5743cca0c1ba3d8dea90
Iraq Jaber <IraqJaber@gmail.com>
2024-12-20 11:16:14 +0800

    refined menu

--- .config/nvim/lua/plugins/z_last.lua
+++ .config/nvim/lua/plugins/z_last.lua
@@ -1,149 +1,50 @@
--- vim.keymap.set("n", "<C-h>", function()
---   local util = require('vim.lsp.util')
---   local params = util.make_position_params()
---   local ms = require('vim.lsp.protocol').Methods
-
---   local method = ms.workspace_symbol
-
---   local a = require 'plenary.async'
---   local tx, rx = a.control.channel.oneshot()
-
---   run = function()
---     a.run(function()
---       -- handler function derived from /neovim/0.10.2_1/share/nvim/runtime/lua/vim/lsp/handlers.lua:M.hover()
---       local handler = function(_, result, ctx, _)
---         if not result then
---           vim.notify('Nothing sent from LSP')
---           return
---         end
---         vim.notify(vim.inspect({ result = result }))
---         print(vim.inspect({ result = result }))
-
---         for _, res in pairs(result) do
---           local len = #res.location.uri
---           local file = string.sub(res.location.uri, len - 6, len - 3)
---           if file == "main" then
---             -- if res.kind == 12 then
---             -- if res.location.range.start.line == 18 then
---             vim.notify(vim.inspect({ ressssss = res }))
---             local kind = vim.lsp.util._get_symbol_kind_name(res.kind)
---             vim.notify(res.kind .. " >>> " .. kind)
---           end
-
---           -- vim.notify(vim.inspect({ file = file }))
---           -- return
---         end
-
---         tx({ type, nil })
---       end
-
---       vim.lsp.buf_request(0, method, { query = "main" }, handler)
---       -- vim.notify(unpack(rx()))
---       -- local res = vim.lsp.buf.workspace_symbol()
---       -- vim.notify(vim.inspect({ res = res }))
---       local t, err = unpack(rx())
---     end)
---   end
---   run()
--- end, { silent = true, noremap = true })
-
-
--- vim.keymap.set("n", "<C-h>", function()
---   -- Get the current buffer and parser
---   local parser = vim.treesitter.get_parser(0, "go") -- Replace "lua" with the language
---   local tree = parser:parse()[1]                    -- Get the syntax tree
-
---   local query = vim.treesitter.query.parse(
---     "go", -- Language
---     [[
---       (package_identifier) @package
---       (function_declaration
---         name: (identifier) @function.name)
---     ]])
-
---   local iter = query:iter_matches(tree:root(), 0, 0, -1)
---   vim.notify(vim.inspect({ iter = iter }))
---   local _, match, _ = iter()
---   -- vim.notify(vim.inspect(match))
---   -- match()
---   -- local i, node = unpack(match)
---   -- vim.notify(vim.inspect(node[1]))
---   -- local capture_name = query.captures[1]                         -- Capture group name
---   -- vim.notify(vim.inspect({ capture_name = capture_name }))
---   -- local element_name = vim.treesitter.get_node_text(match[1], 0) -- Extract matched text
---   -- vim.notify(element_name)
-
---   for _, match, _ in iter do
---     for id, node in pairs(match) do
---       vim.notify(vim.inspect({ id = id }))
---       local capture_name = query.captures[id]                    -- Capture group name
---       local element_name = vim.treesitter.get_node_text(node, 0) -- Extract matched text
---       vim.notify(element_name)
---     end
---   end
--- end, { silent = true, noremap = true })
---
-
--- vim.keymap.set("n", "<C-h>", function()
---   local popup = require("plenary.popup")
-
---   local Win_id
-
---   function ShowMenu(opts, cb)
---     vim.api.nvim_win_set_cursor(0, { 5, 0 })
---     local height = 20
---     local width = 30
---     local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-
---     Win_id = popup.create(opts, {
---       title = "MyProjects",
---       highlight = "MyProjectWindow",
---       line = math.floor(((vim.o.lines - height) / 5.0) - 1),
---       col = math.floor((vim.o.columns - width) / 2),
---       minwidth = width,
---       minheight = height,
---       borderchars = borderchars,
---       callback = cb,
---     })
---     local bufnr = vim.api.nvim_win_get_buf(Win_id)
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "q", ":q<CR>", { silent = false })
---   end
-
---   local opts = {}
---   local cb = function(_, sel)
---     vim.notify(vim.inspect("Wo0p!"))
---   end
---   -- ShowMenu(opts, cb)
--- end, { silent = true, noremap = true })
-
-local ShowMenu = function(opts, cb, lines)
-  -- vim.notify(vim.inspect({ lines = lines }))
-  -- vim.api.nvim_win_set_cursor(0, { 5, 0 })
+local ShowMenu = function(opts)
   local popup = require("plenary.popup")
-  local height = 20
-  local width = 30
+  local height = opts.height
+  local width = opts.width
   local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-  Win_id = popup.create(opts, {
+  winnr = popup.create(opts.lines, {
     title = "Select Go Project",
-    -- highlight = "MyProjectWindow",
+    -- highlight = "Cursor",
     line = math.floor(((vim.o.lines - height) / 5.0) - 1),
     col = math.floor((vim.o.columns - width) / 2),
-    minwidth = width,
-    minheight = height,
+    minwidth = 30,
+    minheight = 13,
     borderchars = borderchars,
-    callback = cb,
+    callback = function(_, sel)
+      vim.notify(vim.inspect("Wo0p! " .. sel))
+    end
   })
-  local bufnr = vim.api.nvim_win_get_buf(Win_id)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "q", ":q<CR>", { silent = false })
-  -- vim.api.nvim_buf_set_option(bufnr, 'guicursor', 'a:Cursor/off')
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+
+  vim.api.nvim_win_set_option(winnr, 'cursorline', true)
+  local bufnr = vim.api.nvim_win_get_buf(winnr)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<Esc>", ":q<CR>", { silent = false })
+
   -- disable insert mode
   vim.cmd("set nomodifiable")
+
+  -- disable the cursor
   -- https://github.com/goolord/alpha-nvim/discussions/75
   local hl = vim.api.nvim_get_hl_by_name('Cursor', true)
-  hl.blend = 0
+  hl.blend = 100
   vim.api.nvim_set_hl(0, 'Cursor', hl)
-  vim.opt.guicursor:append('a:Cursor/lCursor')
+  -- vim.opt.guicursor:append('a:Cursor/lCursor')
+
+  vim.api.nvim_create_autocmd("BufEnter", {
+    buffer = bufnr,
+    callback = function()
+      hl.blend = 100
+      vim.api.nvim_set_hl(0, 'Cursor', hl)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("BufLeave", {
+    buffer = bufnr,
+    callback = function()
+      hl.blend = 0
+      vim.api.nvim_set_hl(0, 'Cursor', hl)
+    end,
+  })
 end
 
 local projects = {}
@@ -219,35 +120,45 @@ vim.keymap.set("n", "<C-h>", function()
   projects = projects_
   -- vim.notify(vim.inspect({ projects = projects }))
   update_project_map(project_root, "asset_generator")
+  -- if not projects_[project_root]['menu'] then
+  --   return
+  -- end
 
   opts = {}
   local cb = function(_, sel)
     vim.notify(vim.inspect("Wo0p!"))
   end
-  ShowMenu(opts, cb, projects_[project_root]['menu'])
-  -- ShowMenu(opts, cb)
+  ShowMenu(projects_[project_root]['menu'])
 end, { silent = true, noremap = true })
 
+-- TODO test this thoroughly
 function update_project_map(project_root, selection)
   local selection_idx = projects[project_root][selection][1]
   if selection_idx == 1 then
     return
   end
-  -- vim.notify(vim.inspect({ projects = projects }))
+
   projects[project_root]['menu'] = nil
   local lines = {}
+  local width = 0
+  local height = 0
+
   for project, proj_details in pairs(projects[project_root]) do
     local proj_idx = proj_details[1]
-    if proj_idx < selection_idx then
+    if proj_idx < selection_idx or proj_idx == 2 then
       proj_idx = proj_idx + 1
       projects[project_root][project][1] = proj_idx
     end
     lines[proj_idx] = project
+    if #project > width then
+      width = #project
+    end
+    height = height + 1
   end
   projects[project_root][selection][1] = 1
   lines[1] = selection
 
-  projects[project_root]['menu'] = lines
+  projects[project_root]['menu'] = { lines = lines, width = width, height = height }
 end
 
 -- function select_project(project_root, selection)
