b9ac2703a7cf57b3f66f6cf1f895db7b37099685
Iraq Jaber <IraqJaber@gmail.com>
2025-01-06 12:47:58 +0800

    added range to comment out line and paste, added new keymaps for B + E and misc

--- .config/nvim/lua/plugins/comments.lua
+++ .config/nvim/lua/plugins/comments.lua
@@ -13,19 +13,57 @@ return {
       vim.api.nvim_feedkeys("gcc", 'm', false)
     end, { silent = true, noremap = true })
 
-    vim.keymap.set("n", "gcn", function()
-      if vim.api.nvim_get_current_line() == "" then
+    vim.api.nvim_create_user_command("CommentAndPasteLines", function(opts)
+      local start_line = opts.line1
+      local end_line = opts.line2
+      local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+
+      vim.notify(vim.inspect({ "start_line, end_line>>>", start_line, end_line }))
+
+      if start_line == end_line then
+        -- if vim.api.nvim_get_current_line() == "" then
+        if lines[1] == "" then
+          return
+        end
+        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+        -- copy line
+        vim.fn.setreg('a', lines)
+
+        -- comment out line
+        vim.api.nvim_feedkeys("gcl", 'x', true)
+
+        -- move to next line
+        vim.api.nvim_feedkeys("o", 'x', true)
+
+        -- paste lins
+        vim.api.nvim_paste(vim.fn.getreg('a'), true, -1)
+
+        -- set cursor position
+        vim.api.nvim_win_set_cursor(0, { row + 1, col })
         return
+      else
+        -- copy lines
+        local text_to_copy = table.concat(lines, "\n")
+        vim.fn.setreg('a', text_to_copy)
+
+        -- comment out lines
+        lines = end_line - start_line
+        vim.api.nvim_feedkeys("V" .. lines .. "j", 'x', true)
+        vim.api.nvim_feedkeys("gcl", 'x', true)
+
+        -- move to first line after selected lines
+        vim.api.nvim_win_set_cursor(0, { end_line, 0 })
+        vim.api.nvim_feedkeys("o", 'x', true)
+
+        -- paste lins
+        vim.api.nvim_paste(vim.fn.getreg('a'), true, -1)
+        vim.api.nvim_win_set_cursor(0, { end_line + 1, 0 })
+        vim.api.nvim_feedkeys("I", 'x', true)
+        vim.api.nvim_feedkeys("l", 'x', true)
       end
-      -- get curosr location
-      local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-      -- get line
-      vim.api.nvim_feedkeys("\"ayy", 'x', true)
-      vim.api.nvim_feedkeys("gcl", 'x', true)
-      -- paste line
-      vim.api.nvim_feedkeys("\"ap", 'x', true)
-      -- set curosr location
-      vim.api.nvim_win_set_cursor(0, { row + 1, col })
-    end, { silent = true, noremap = true })
+    end, { force = true, range = true })
+
+    vim.keymap.set({ "n", "v" }, "gcn", ":CommentAndPasteLines<CR>", { silent = true, noremap = true })
   end
 }
