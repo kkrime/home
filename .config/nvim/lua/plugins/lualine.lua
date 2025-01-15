022dd73de7f47cd362e013130fef335114e06bfc
Iraq Jaber <IraqJaber@gmail.com>
2025-01-15 11:46:47 +0700

    added markdown.lua

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,32 +3,32 @@ return {
     "nvim-lualine/lualine.nvim",
 
     config = function()
-      local custom_gruvbox         = require('lualine.themes.gruvbox')
-      custom_gruvbox.normal.a.bg   = 'green'
-      custom_gruvbox.normal.a.fg   = 'black'
-      custom_gruvbox.insert.a.bg   = 'red'
-      custom_gruvbox.insert.a.fg   = 'black'
-      custom_gruvbox.command.a.bg  = 'yellow'
-      custom_gruvbox.command.a.fg  = 'black'
-      custom_gruvbox.visual.a.fg   = 'black'
+      local custom_gruvbox             = require('lualine.themes.gruvbox')
+      custom_gruvbox.normal.a.bg       = 'green'
+      custom_gruvbox.normal.a.fg       = 'black'
+      custom_gruvbox.insert.a.bg       = 'red'
+      custom_gruvbox.insert.a.fg       = 'black'
+      custom_gruvbox.command.a.bg      = 'yellow'
+      custom_gruvbox.command.a.fg      = 'black'
+      custom_gruvbox.visual.a.fg       = 'black'
 
-      custom_gruvbox.normal.c.bg   = custom_gruvbox.normal.a.bg
-      custom_gruvbox.normal.c.fg   = custom_gruvbox.normal.c.bg
-      custom_gruvbox.insert.c.bg   = custom_gruvbox.insert.a.bg
-      custom_gruvbox.insert.c.fg   = custom_gruvbox.insert.c.bg
-      custom_gruvbox.command.c.bg  = custom_gruvbox.command.a.bg
-      custom_gruvbox.command.c.fg  = custom_gruvbox.command.c.bg
-      custom_gruvbox.visual.c.bg   = custom_gruvbox.visual.a.bg
-      custom_gruvbox.visual.c.fg   = custom_gruvbox.visual.c.bg
-      custom_gruvbox.inactive.c.fg = '#3c3836'
+      custom_gruvbox.normal.c.bg       = custom_gruvbox.normal.a.bg
+      custom_gruvbox.normal.c.fg       = custom_gruvbox.normal.c.bg
+      custom_gruvbox.insert.c.bg       = custom_gruvbox.insert.a.bg
+      custom_gruvbox.insert.c.fg       = custom_gruvbox.insert.c.bg
+      custom_gruvbox.command.c.bg      = custom_gruvbox.command.a.bg
+      custom_gruvbox.command.c.fg      = custom_gruvbox.command.c.bg
+      custom_gruvbox.visual.c.bg       = custom_gruvbox.visual.a.bg
+      custom_gruvbox.visual.c.fg       = custom_gruvbox.visual.c.bg
+      custom_gruvbox.inactive.c.fg     = '#3c3836'
 
-      custom_gruvbox.normal['y']   = { bg = custom_gruvbox.normal.a.bg, fg = 'white', gui = 'bold' }
-      custom_gruvbox.insert['y']   = { bg = custom_gruvbox.insert.a.bg, fg = 'black', gui = 'bold' }
-      custom_gruvbox.command['y']  = { bg = custom_gruvbox.command.a.bg, fg = 'black', gui = 'bold' }
-      custom_gruvbox.visual['y']   = { bg = custom_gruvbox.visual.a.bg, fg = 'black', gui = 'bold' }
-      custom_gruvbox.inactive['y'] = { bg = '#3c3836', fg = 'white', gui = 'bold' }
+      custom_gruvbox.normal['y']       = { bg = custom_gruvbox.normal.a.bg, fg = 'white', gui = 'bold' }
+      custom_gruvbox.insert['y']       = { bg = custom_gruvbox.insert.a.bg, fg = 'black', gui = 'bold' }
+      custom_gruvbox.command['y']      = { bg = custom_gruvbox.command.a.bg, fg = 'black', gui = 'bold' }
+      custom_gruvbox.visual['y']       = { bg = custom_gruvbox.visual.a.bg, fg = 'black', gui = 'bold' }
+      custom_gruvbox.inactive['y']     = { bg = '#3c3836', fg = 'white', gui = 'bold' }
 
-      local lualine_b              = {
+      local lualine_b                  = {
         {
           'filename',
           color = function()
@@ -50,7 +50,7 @@ return {
         },
       }
 
-      local lualine_c              = {
+      local lualine_c                  = {
         {
           function()
             return " "
@@ -59,20 +59,31 @@ return {
         },
       }
 
-      local lualine_x              = {
+      local go_get_current_buildtarget = require('go.buildtargets').get_current_buildtarget
+      local lualine_x                  = {
         {
           'filetype',
+          fmt = function(str)
+            if str == 'go' then
+              local buildtarget = go_get_current_buildtarget()
+              if buildtarget then
+                return buildtarget
+              end
+            end
+            return str
+          end,
+
           color = { fg = 'white', bg = 'black', gui = 'bold' }
         },
       }
 
-      local lualine_z              = {
+      local lualine_z                  = {
         {
           'location',
           color = { fg = 'white', bg = 'black', gui = 'bold' }
         },
       }
-      local lualine_y              = { 'progress' }
+      local lualine_y                  = { 'progress' }
 
       require('lualine').setup {
         options = {
@@ -106,3 +117,4 @@ return {
     end
   },
 }
+
