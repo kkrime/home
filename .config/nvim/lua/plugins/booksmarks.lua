64ec2307577f2f72b1250591983a22d89b1a1ed7
Iraq Jaber <IraqJaber@gmail.com>
2025-01-29 15:42:24 +0000

    fixed issue in bookmarks.lua

--- .config/nvim/lua/plugins/booksmarks.lua
+++ .config/nvim/lua/plugins/booksmarks.lua
@@ -8,17 +8,21 @@ return {
       local ts = require('telescope')
       ts.load_extension('bookmarks')
 
-      local bm = require "bookmarks"
-      local map = vim.keymap.set
-      map("n", "mm", bm.bookmark_toggle)                 -- add or remove bookmark at current line
-      map("n", "mi", bm.bookmark_ann)                    -- add or edit mark annotation at current line
-      map("n", "mx", bm.bookmark_clean)                  -- clean all marks in local buffer
-      map("n", "mn", bm.bookmark_next)                   -- jump to next mark in local buffer
-      map("n", "mp", bm.bookmark_prev)                   -- jump to previous mark in local buffer
-      map("n", "ml", function()
-        require('telescope').extensions.bookmarks.list() -- show marked file list in quickfix window
-      end)
-      bm.setup()
+      require('bookmarks').setup {
+        on_attach = function(bufnr)
+          local bm = require "bookmarks"
+          local map = vim.keymap.set
+          map("n", "mm", bm.bookmark_toggle)                 -- add or remove bookmark at current line
+          map("n", "mi", bm.bookmark_ann)                    -- add or edit mark annotation at current line
+          map("n", "mx", bm.bookmark_clean)                  -- clean all marks in local buffer
+          map("n", "mn", bm.bookmark_next)                   -- jump to next mark in local buffer
+          map("n", "mp", bm.bookmark_prev)                   -- jump to previous mark in local buffer
+          map("n", "ml", function()
+            require('telescope').extensions.bookmarks.list() -- show marked file list in quickfix window
+          end)
+          -- bm.setup()
+        end
+      }
     end
   },
 };
