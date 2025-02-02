1833fc83080a77c1aad7f5cc758dec3f37619cbe
Iraq Jaber <IraqJaber@gmail.com>
2025-02-02 15:19:54 +0000

    adding db.lua

---
+++ .config/nvim/lua/plugins/db.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup {
+        sources = {
+          require("dbee.sources").FileSource:new(vim.fn.expand("$HOME") .. "/.persistence.json"),
+        },
+      }
+    end,
+  },
+}
