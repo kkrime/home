101d04cace42b67c02f6ec8080c6f83ef37b0f42
Iraq Jaber <iraq+github@zitadel.com>
2025-03-31 17:47:50 +0100

    misc

--- .config/nvim/lua/plugins/db.lua
+++ .config/nvim/lua/plugins/db.lua
@@ -12,6 +12,7 @@ return {
         default_connection = 'zitadel1',
         result = {
           focus_result = false,
+          page_size = 100000
         },
         sources = {
           require("dbee.sources").FileSource:new(vim.fn.expand("$HOME") .. "/.persistence.json"),
