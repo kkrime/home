e962a1524363758aa17e3e2acf32b7b7508658cf
Iraq Jaber <iraq+github@zitadel.com>
2025-06-15 17:13:27 +0200

    fix to highlight.lua

--- .config/nvim/lua/plugins/highlight.lua
+++ .config/nvim/lua/plugins/highlight.lua
@@ -1,5 +1,6 @@
 local function search(direction)
   local match = vim.fn.getmatches()
+  -- vim.notify(vim.inspect({ "match", match }))
   if next(match) == nil then
     local search_pattern = vim.fn.getreg('/')
     if search_pattern == '@@' then
@@ -7,12 +8,8 @@ local function search(direction)
     end
     vim.cmd('normal! ' .. direction)
   else
+    vim.notify("branch")
     local word = match[1].pattern
-    vim.notify(word)
-    vim.notify(vim.inspect({ "match[1].pattern", match[1].pattern }))
-
-    vim.notify(vim.inspect({ "word", word }))
-
     vim.fn.setreg('/', word)
     vim.cmd('normal! ' .. direction)
   end
@@ -27,15 +24,37 @@ vim.keymap.set("n", "N", function()
 end, { silent = true, noremap = true })
 
 
+-- vim.api.nvim_set_hl(0, "MyHighlight", {
+--   fg = "#ffffff", -- white text
+--   bg = "#005f87", -- dark blue background
+--   bold = true,    -- bold text
+--   italic = false,
+--   underline = false,
+-- })
 
 vim.keymap.set("n", "(", function()
+  vim.cmd("nohlsearch")
+  -- vim.cmd("match none")
   local word = vim.fn.expand("<cword>")
   vim.cmd("match Search /\\<" .. word .. "\\>/")
+  -- vim.cmd("match MyHighlight /\\<" .. word .. "\\>/")
 end, { noremap = true, silent = true })
 
 vim.keymap.set("n", ")", function()
   vim.cmd("nohlsearch")
   vim.cmd("match none")
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "*", function()
+  -- vim.cmd("nohlsearch")
+  vim.cmd("match none")
+  vim.cmd('normal! *')
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "#", function()
+  -- vim.cmd("nohlsearch")
+  vim.cmd("match none")
+  vim.cmd('normal! #')
 end, { noremap = true, silent = true })
 
 
