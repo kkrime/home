889a9786ad6e7eebf0677caf69ea84926e28619a
Iraq Jaber <iraq+github@zitadel.com>
2025-07-21 14:13:46 +0100

    misc

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -1,12 +1,12 @@
 local divide_height_by = 5
 
-vim.keymap.set("n", "<C-d>", function()
+vim.keymap.set({ "n", "v" }, "<C-d>", function()
   local win_height = vim.api.nvim_win_get_height(0)
   local count = math.floor(win_height / divide_height_by)
   vim.cmd("normal! " .. count .. "j")
 end, { silent = true, noremap = true })
 
-vim.keymap.set("n", "<C-u>", function()
+vim.keymap.set({ "n", "v" }, "<C-u>", function()
   local win_height = vim.api.nvim_win_get_height(0)
   local count = math.floor(win_height / divide_height_by)
   vim.cmd("normal! " .. count .. "k")
