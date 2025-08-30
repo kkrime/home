c420a5d9d22cc00b2b10055723f1867e4b520e8c
Iraq Jaber <iraq+github@zitadel.com>
2025-08-30 16:25:05 +0100

    updated navigation with functionjump() + added :Z command

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -1,5 +1,52 @@
-local divide_height_by = 5
+local function function_jump(direction)
+  local node = vim.treesitter.get_node()
 
+  local source_file = 95
+  local function_declaration = 107
+  local method_declaration = 108
+  local parent
+  while (true) do
+    if node == nil or node:symbol() == source_file then
+      break
+    end
+
+    parent = node:parent()
+    local function_node
+
+    if node:symbol() == function_declaration or node:symbol() == method_declaration then
+      function_node = node
+    elseif parent:symbol() == function_declaration or parent:symbol() == method_declaration then
+      function_node = parent
+    end
+    if function_node ~= nil then
+      local start_row, start_col, end_row, end_col = function_node:range()
+      local row, col
+      if direction == 'k' then
+        row = start_row + 1
+        col = start_col + 1
+      elseif direction == 'j' then
+        row = end_row + 1
+        col = end_col
+      end
+      if col == 1 then
+        vim.cmd("normal! m'")
+        vim.api.nvim_win_set_cursor(0, { row, col })
+        break
+      end
+    end
+    node = parent
+  end
+end
+
+vim.keymap.set({ "n", "v" }, "<C-k>", function()
+  function_jump('k')
+end, { silent = true, noremap = true })
+
+vim.keymap.set({ "n", "v" }, "<C-j>", function()
+  function_jump('j')
+end, { silent = true, noremap = true })
+
+local divide_height_by = 5
 vim.keymap.set({ "n", "v" }, "<C-d>", function()
   local win_height = vim.api.nvim_win_get_height(0)
   local count = math.floor(win_height / divide_height_by)
--- .config/nvim/lua/plugins/z_last.lua
+++ .config/nvim/lua/plugins/z_last.lua
@@ -1,54 +1,9 @@
--- vim.keymap.set("n", "<C-h>", function()
---   require("buildtargets").select_buildtarget()
--- end)
+vim.api.nvim_create_user_command('Z', function()
+  vim.cmd("e ~/.config/nvim/lua/plugins/z_last.lua")
+end, {})
 
--- local target = nil
-
--- vim.keymap.set("n", "<C-h>", function()
-
-local source = ".persistence.json"
-local zitadel_db = "/tmp/zitadel_db"
-vim.keymap.set({ "n", "v" }, "<C-h>", function()
-  local handler = require("dbee.api.core")
-
-  -- 1. get current connection
-  local current_connection = handler.get_current_connection()
-  if current_connection == nil then
-    vim.notify("can't read current connection", vim.log.levels.ERROR)
-    return
-  end
-
-  -- 2. read zitadel_db
-  local ok, db = pcall(vim.fn.readfile, zitadel_db)
-  if not ok then
-    vim.notify("can't read " .. zitadel_db, vim.log.levels.ERROR)
-    return
-  end
-  db = db[1]
-
-  -- 3. check if we're already connected to db
-  if current_connection ~= nil and current_connection.name == db then
-    return
-  end
-
-  -- 4. check if db connection already exists
-  local found
-  local connections = handler.source_get_connections(source)
-  for _, conn in ipairs(connections) do
-    if conn.name == db then
-      found = true
-      break
-    end
-  end
-
-  -- 5. add db and connect
-  if not found then
-    vim.notify("DB NOT FOUND ADDING " .. db, vim.log.levels.INFO)
-    handler.source_add_connection(source,
-      { name = db, type = "postgres", url = "postgres://zitadel:zitadel@localhost:5432/" .. db .. "?sslmode=disable" })
-  else
-    handler.set_current_connection(db)
-  end
+vim.keymap.set("n", "<C-h>", function()
+  require("buildtargets").select_buildtarget()
 end)
 
 return {}
